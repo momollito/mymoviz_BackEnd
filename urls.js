5baf86e3603af0c1d72cd22d378595a53016e9e4
Mathis Rosset <rosset.mathis@gmail.com>
2023-04-19 14:31:41 +0200

    deploy

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'mymoviz-back-end.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'mymoviz-front-end.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
